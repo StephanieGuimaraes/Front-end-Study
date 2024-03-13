493d4ea1612f5476d0fc63f0d55bde2d3b56fe63
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-03-13 20:16:25 -0300

    feat: fim do tópico "variaveis"

---
+++ 01-fundamentos-JS/Teoria/Variaveis.js
@@ -0,0 +1,10 @@
+/*
+ Var é global
+  * Ocorre o hoisting (elevação) por de baixo dos panos o 
+    Javascript pega a variavel do tipo "var" e eleva para um nívle global
+*/
+
+/*
+    * Const e let são locais e só funcionam no escopo onde foram criadas.
+    Mas o let pode buscar um valor de uma variavel nos níveis de escopo acima
+*/
