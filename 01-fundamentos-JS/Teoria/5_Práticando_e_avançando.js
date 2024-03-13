2ada6e26dbf6ffbc3eba06b992369dcf7078dc0f
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-03-13 20:42:33 -0300

    feat: fim do tópico "pra ticando e avançando"

---
+++ 01-fundamentos-JS/Teoria/5_Práticando_e_avançando.js
@@ -0,0 +1,22 @@
+/*
+    Interpolando valores com template literals os template strings
+*/
+
+// let age, isHuman, name
+
+// age = 15
+// isHuman = true
+// name = "Steph"
+
+// console.log(`a ${name} tem ${age} anos, ela é humana? ${isHuman}!`)
+
+let student = {
+    name: "Steph",
+    age: 28,
+    weight: 50.5,
+    isHuman: true,
+}
+
+let students = [student, student]
+
+console.log(students.length)
