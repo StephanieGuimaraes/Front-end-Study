9e6c389171d3c017165e77719bbfb66dd561cccb
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-03-14 17:13:11 -0300

    feat: conteudo de functions 1

---
+++ 01-fundamentos-JS/Teoria/6_Functions/functions.js
@@ -0,0 +1,12 @@
+//    function expression
+//    fucntion anonymous
+
+let subject
+const think = function(subject){
+    subject = "study"
+    return subject
+}
+
+console.log(subject)
+console.log(think(subject))
+console.log(subject)
