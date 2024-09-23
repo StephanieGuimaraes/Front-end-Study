c9bca0fc882dfb2a371044221a6ba1efbdd63805
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-09-23 10:08:32 -0300

    feat: exercicio Operacoes Matematicas

---
+++ 01-fundamentos-JS/Explorer_Int_Program_Log_JS/operacoesMatematicas.js
@@ -0,0 +1,8 @@
+let firstNumber = prompt("Digite primeiro numero")
+let secondNumber = prompt("Digite primeiro numero")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+const resultado = firstNumber % secondNumber
+
+alert("Resultado: " + resultado)
