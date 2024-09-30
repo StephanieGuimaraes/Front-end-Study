abef872d6dff653201bfa0f725fda1dabf0cec29
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-09-30 14:42:09 -0300

    feat: inicio exercicio IMC

---
+++ 01-fundamentos-JS/Explorer_Int_Program_Log_JS/calcIMC.js
@@ -0,0 +1,20 @@
+const patients = [
+    {
+        nome: "Carlos",
+        idade: "33",
+        peso: 70,
+        height: 190,
+    },
+    {
+        nome: "Maria",
+        idade: "43",
+        peso: 80,
+        height: 160,
+    },
+    {
+        nome: "Bia",
+        idade: "23",
+        peso: 50,
+        height: 155,
+    }
+]
